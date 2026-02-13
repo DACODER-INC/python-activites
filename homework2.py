bf0f3c7774276f37afa2266c78d0eb1a77a001a8
DACODER-INC <cool.navjyot@gmail.com>
2026-02-13 17:17:51 -0500

    homework 2

---
+++ homework2.py
@@ -0,0 +1,12 @@
+print('this is in order of mm/dd/yyyy')
+friend_1 =0o1,0o2,2015
+friend_2 = 0o3,0o6,2017
+friend_3 = 11,13,2015
+friend_4 = 0o4,0o5,2015
+friend_5 = 0o5,28,2015
+print(friend_1)
+print(friend_2)
+print(friend_3)
+print(friend_4)
+print(friend_5)
+ 
