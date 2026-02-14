95ac21fe934d28d438972948abeb4fc32895dcca
DACODER-INC <cool.navjyot@gmail.com>
2026-02-14 14:45:07 -0500

    bruh

---
+++ 5.py
@@ -0,0 +1,14 @@
+a = 108298.39
+int_a = int(a)
+print('The value of a is ', a)
+print('The value of int_a is ', int_a)
+print('type of a is ', type(a))
+print('type of int_a is ', type(int_a))
+b = -10.757
+int_b = int(b)
+print('The value of b is ', b)
+print('The value of int_b is ', int_b)
+print('type of b is ', type(b))
+print('type of int_b is ', type(int_b))
+
+
