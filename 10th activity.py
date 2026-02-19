18692b6fdbdd511db04352f580bb23152c97c7ec
DACODER-INC <cool.navjyot@gmail.com>
2026-02-19 16:45:59 -0500

    i finally remembred the count

---
+++ 10th activity.py
@@ -0,0 +1,9 @@
+tree1 = 194
+tree2 = 205
+tree3 = 210
+tree4 = 947
+tree5 = 836
+sum_of_trees = tree1 + tree2 + tree3 + tree4 + tree5
+print(' The sum of the heights of the trees is : ' + str(sum_of_trees))
+average_height = sum_of_trees / 5
+print(' The average height of the trees is : ' + str(average_height))
