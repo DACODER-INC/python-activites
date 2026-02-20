b76af73e0cb6b049b2105796fe7c870a46dd756f
DACODER-INC <cool.navjyot@gmail.com>
2026-02-20 17:50:01 -0500

    t

---
+++ 13th thingy.py
@@ -0,0 +1,7 @@
+number = int(input(' Enter a random number : '))
+result = number ** 0.5
+rounded = round(result)
+print(' The square root of the number is : ' + str(result))
+print(' The rounded value of the square root is : ' + str(rounded))
+
+
