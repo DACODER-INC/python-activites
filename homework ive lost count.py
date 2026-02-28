92e8f0a9013de747f9b20dd706e7740bde8e3170
DACODER-INC <cool.navjyot@gmail.com>
2026-02-28 15:35:55 -0500

    w

---
+++ homework ive lost count.py
@@ -0,0 +1,7 @@
+temp =  int(input(' what temparature is it outside : '))
+if temp > 20:
+    print(' It is suitable for wearing light clothes ')
+else:
+    print(' It is not suitable for wearing light clothes ')
+if temp > 35:
+    print(' It is too hot outside ')
