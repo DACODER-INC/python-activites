8414e0984cbcb0010c0ab3bb840a6d6e101304e9
DACODER-INC <cool.navjyot@gmail.com>
2026-03-04 16:56:15 -0500

    f

---
+++ 20th_milestone.py
@@ -0,0 +1,16 @@
+a = 9484
+b = 9484
+c = 87878768978987678767
+print(a == b , a != c)
+
+
+
+number = int(input('Enter a number: '))
+if number % 2 != 0:
+    print('The number is odd ')
+else:
+    print('The number is even ')
+    
+
+
+
