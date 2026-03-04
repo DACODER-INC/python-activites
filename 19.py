f51f469991dae94bf0b491d56edb88920398e803
DACODER-INC <cool.navjyot@gmail.com>
2026-03-04 16:42:01 -0500

    gngnmgmgjjgmggbngngjbng

---
+++ 19.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print(' all the no. are boolean true ')
+else:  
+    print(' all the no. are not boolean true ')
+     
+
+
+d = -10
+e = 10
+f = -4968598758768578578678578
+if d > 0 or e > 0 or f > 0:
+    print(' at least one of the no. is positive ')
+else:
+    print(' all the no. are negative ')
