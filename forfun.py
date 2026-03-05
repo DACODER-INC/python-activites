0d8b760cd84cf5e6744a1d0358822f2913785ffa
DACODER-INC <cool.navjyot@gmail.com>
2026-03-04 20:28:23 -0500

    hjiuhgh

---
+++ forfun.py
@@ -0,0 +1,36 @@
+age = int(input('Enter your age: '))
+if age == 5:
+    print('You are in junior kindergarten or senior kindergarten ')
+elif age == 6:
+    print('You are in grade 1 ')
+elif age ==7:
+    print('You are in grade 2 ')
+elif age == 8:
+    print('You are in grade 3 ')
+elif age == 9:
+    print('You are in grade 4 ')
+elif age == 10:
+    print('You are in grade 5 ')
+elif age == 11:
+    print('You are in grade 6 ')
+elif age == 12:
+    print('You are in grade 7 ')
+elif age == 13:
+    print('You are in grade 8 ')
+elif age == 14:
+    print('You are in grade 9 ')
+elif age == 15:
+    print('You are in grade 10 ')
+elif age == 16:
+    print('You are in grade 11 ')
+elif age == 17:
+    print('You are in grade 12 ')
+elif age == 18:
+    print('You are in university and you have a job ')
+elif age == 19:
+    print('You can drink and vote in ontario ')
+else:
+    print('You are an adult and you can do whatever you want ')
+if age > 80:
+    print('You are probably dead ')
+
