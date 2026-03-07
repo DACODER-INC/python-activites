68d17b19acdd571bf77f2b3002ac3de7f5890949
DACODER-INC <cool.navjyot@gmail.com>
2026-03-07 14:42:47 -0500

    f

---
+++ 23.py
@@ -0,0 +1,21 @@
+a = 10
+b = 7
+a >> 1
+a >> 2
+a << 1
+a << 2
+
+b >> 1
+b >> 2
+b << 1
+b << 2
+print(a >> 1)
+print(a >> 2)
+print(a << 1)
+print(a << 2)
+
+
+print(a << 1)
+print(a << 2)
+print(a >> 1)
+print(a >> 2)
