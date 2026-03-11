453194191fa900ccf5d97145a50992ae62e1332a
DACODER-INC <cool.navjyot@gmail.com>
2026-03-11 16:46:23 -0400

    gl

---
+++ 25th classroom activity.py
@@ -0,0 +1,16 @@
+a = 3
+b = 9
+c = 6
+d = 1
+e = 4
+f = (a + b) ** (c - d) / e
+print(f)
+
+name = input("Enter your name: ").lower()
+age = int(input("Enter your age: "))
+debit_card = str(input('what is your debit card provider?'))
+if name == 'nav' or name == 'john' and age >= 10 and debit_card == 'mydoh':
+    print("Hello, Nav!")
+else:
+    print('Hacker be banished from navs or johns account and be thrown into the recycling bin')
+
