69b91e785c852e93640225c77678f1983562a27b
DACODER-INC <cool.navjyot@gmail.com>
2026-03-11 16:56:08 -0400

    u

---
+++ 27.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+sum = mean1 * total_numbers
+print("The sum of the numbers is: " + str(sum))
+num = sum - (wrong_number - correct_number)
+print("The correct sum of the numbers is: " + str(num))
+mean2 = num / total_numbers
+print("The correct mean of the numbers is: " + str(mean2))
