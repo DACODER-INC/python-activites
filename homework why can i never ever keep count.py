38d6b192b4f68519334c85796ad31947460593ce
DACODER-INC <cool.navjyot@gmail.com>
2026-03-14 10:05:13 -0400

    ifu8ithkg

---
+++ homework why can i never ever keep count.py
@@ -0,0 +1,13 @@
+x = (input("Enter a something: "))
+y = (input("Enter another something: "))
+z = (input("Enter a third something: "))
+
+temp = x
+x = y
+y = z
+z = temp
+print("After swapping:")
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
