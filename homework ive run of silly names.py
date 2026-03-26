4b88e7e98497212777d53d6fcdd0b5fd12b93150
DACODER-INC <cool.navjyot@gmail.com>
2026-03-26 15:36:55 -0400

    t

---
+++ homework ive run of silly names.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+power = int(input('enter the number of times you want to multiply the number by itself: '))
+result = num ** power
+print(f'{num} to the power of {power} is: {result}')
