7e938f3cc24d54efd43418c0af65e7424f6ad94a
DACODER-INC <cool.navjyot@gmail.com>
2026-04-22 16:32:31 -0400

    .

--- forty third project of mine.py
+++ forty third project of mine.py
@@ -1,7 +1,7 @@
 numofrows = int(input("Enter the number of rows for the floyds triangle: "))
 num = 1
-for i in range(1, numofrows + 1):
-    for j in range(1, i + 1):
+for i in range(numofrows):
+    for j in range(i + 1):
         print(num, end=" ")
         num += 1
     print()
