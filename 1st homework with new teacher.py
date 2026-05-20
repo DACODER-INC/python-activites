4ac32ed719854011aa4dc3a98a64b80d9b01a489
DACODER-INC <cool.navjyot@gmail.com>
2026-05-20 16:26:00 -0400

    j

---
+++ 1st homework with new teacher.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.screensize(200, 200)
+turtle.Screen().bgcolor('aqua')
+square = turtle.Turtle()
+square.pendown
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+turtle.done()
