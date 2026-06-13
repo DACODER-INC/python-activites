cd0d89404bd1f4254e5e8bb3313d712381a9c469
DACODER-INC <cool.navjyot@gmail.com>
2026-06-13 15:16:52 -0400

    jhm

---
+++ 53.py
@@ -0,0 +1,11 @@
+def factorial(number):
+    if number == 0 or number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
+    
+print(factorial(5))
+print(factorial(6))
+print(factorial(0))
+print(factorial(1))
+print(factorial(87))
