90181210db16963ae46023311023808ee8067817
DACODER-INC <cool.navjyot@gmail.com>
2026-07-07 15:48:46 -0400

    cdcdcdcd

---
+++ homework i have no idea what i am doing.py
@@ -0,0 +1,9 @@
+import math
+
+angle_degrees = 45
+angle_degrees = math.degrees(angle_degrees)
+sin_val = math.sin(angle_degrees)
+cos_val = math.cos(angle_degrees)
+tan_val = math.tan(angle_degrees)   
+print('The order of the numbers is sin then cos then tan')
+print(sin_val, cos_val, tan_val)
