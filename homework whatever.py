f065a6ccabb04d0f88d66c81f609031185726a7a
DACODER-INC <cool.navjyot@gmail.com>
2026-08-21 17:57:37 -0400

    jhihi

--- homework whatever.py
+++ homework whatever.py
@@ -5,7 +5,7 @@ alpha = string.ascii_letters
 numbers = string.digits
 password =''
 all_charc = alpha + numbers
-for i in range(10):
+for i in range(12):
     random_choice = random.choice(all_charc)
     password = password + random_choice
 print('Your password is',password)
