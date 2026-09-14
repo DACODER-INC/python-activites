490d4c5b96a9abbf39a223ff92633b1f8be40ddb
DACODER-INC <cool.navjyot@gmail.com>
2026-09-14 17:15:57 -0400

    h yh yjyj

--- homework one more class before the end of the module.py
+++ homework one more class before the end of the module.py
@@ -1,3 +1,60 @@
 from abc import ABC, abstractmethod
 
-cla
+class SmartDevice(ABC):
+    def show_device(self, name):
+        print('Device name', name)
+    
+
+    @abstractmethod
+    def turn_on(self):
+        pass
+
+
+class SmartTV(SmartDevice):
+    def turn_on(self):
+        print('Smart tv is now ON')
+
+
+class SmartLights(SmartDevice):
+    def turn_on(self):
+        print('All the lights in the house are now ON')
+
+
+class SmartStove(SmartDevice):
+    def turn_on(self):
+        print('The Smart stove is now ON')
+
+
+tv = SmartTV()
+lights = SmartLights()
+stove = SmartStove()
+
+tv.show_device('Living Room TV')
+tv.turn_on()
+
+lights.show_device('Lights')
+lights.turn_on()
+
+stove.show_device('The cooking stove')
+stove.turn_on()
+
+
+class SecurityCam:
+    def check_status(self):
+        print('The security cam is now watching the Smart Stove')
+
+
+class DoorLock:
+    def check_status(self):
+        print('The door lock is secure and locked')
+
+
+devices = [SecurityCam(), DoorLock()]
+ 
+print("")
+print("===== SMART DEVICE STATUS =====")
+ 
+for device in devices:
+    device.check_status()
+ 
+print("===============================")
